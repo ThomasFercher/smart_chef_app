41d931f2b611b8dd4c9b8611a048520524db17bc
Thomas Fercher <thomas.fercher@outlook.com>
2023-03-14 16:52:25 +0100

    Add MacOS

--- lib/content/services/api_service.dart
+++ lib/content/services/api_service.dart
@@ -5,27 +5,29 @@ import '../model/category.dart';
 import '../model/ingredient.dart';
 
 class ApiService {
-  String endpoint = "info.smartchef.ai";
+  String endpoint = "https://info.smartchef.ai";
   String ingredient = "/ingredients";
   String category = "/categories";
 
   Future<List<Ingredient>> fetchIngredient() async {
-    final response = await http.get(
-      Uri.https(endpoint, ingredient),
-      headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json',
-      },
-    );
-    if (response.statusCode == 200) {
-      // If the server did return a 200 OK response,
-      // then parse the JSON.
-      final List result = jsonDecode(response.body);
-      return result.map((e) => Ingredient.fromJson(e)).toList();
-    } else {
-      // If the server did not return a 200 OK response,
-      // then throw an exception.
-      throw Exception('Failed to load ingredient');
+    final uri = Uri.parse('$endpoint$ingredient');
+    try {
+      final response = await http.get(
+        uri,
+      );
+      if (response.statusCode == 200) {
+        // If the server did return a 200 OK response,
+        // then parse the JSON.
+        final List result = jsonDecode(response.body);
+        return result.map((e) => Ingredient.fromJson(e)).toList();
+      } else {
+        // If the server did not return a 200 OK response,
+        // then throw an exception.
+        throw Exception('Failed to load ingredient');
+      }
+    } catch (e) {
+      print(e);
+      rethrow;
     }
   }
 
